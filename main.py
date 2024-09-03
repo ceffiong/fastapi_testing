ea264e9f19ab203df21de3e9679af438091cd52f
effiongcharles <talkwithcharles@gmail.com>
2024-09-03 14:49:02 +0200

    updated requirements

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from schema import User
+
+app = FastAPI()
+
+
+@app.post("/user/", response_model=User)
+async def create_user(user: User):
+    return user
+
+
+@app.get("/users/", response_model=list[User])
+async def get_users():
+    return [
+        User(name="John Doe", age=20, score=100),
+        User(name="Mary Doe", age=19, score=100),
+        User(name="Anna Thomas", age=19, score=100),
+    ]
