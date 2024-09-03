2623a2233d878d2b3d01234e902765fc19b47f1c
effiongcharles <talkwithcharles@gmail.com>
2024-09-03 14:42:52 +0200

    first commit

---
+++ test_main.py
@@ -0,0 +1,39 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_create_user():
+    user = {
+        "name": "John Doe",
+        "age": 20,
+        "score": 100
+    }
+    response = client.post("/user/", json=user)
+    assert response.status_code == 200
+    assert response.json() == user
+
+
+def test_get_users():
+    response = client.get("/users/")
+    assert response.status_code == 200
+
+    users = [
+        {
+            "name": "John Doe",
+            "age": 20,
+            "score": 100
+        },
+        {
+            "name": "Mary Doe",
+            "age": 19,
+            "score": 100
+        },
+        {
+            "name": "Anna Thomas",
+            "age": 19,
+            "score": 100
+        }
+    ]
+    assert response.json() == users
